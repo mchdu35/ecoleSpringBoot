25d4b61aa85bc0acdac02afcc65dd337f83e3763
Mathieu <mathieu.chretien.web@gmail.com>
2019-05-02 23:25:34 +0200

    Ajout des sources SpringBoot.

---
+++ src/main/java/org/chretien/mathieu/ecoleServer/EcoleServerApplication.java
@@ -0,0 +1,13 @@
+package org.chretien.mathieu.ecoleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoleServerApplication.class, args);
+	}
+
+}
